4c1df9b9c77c94728836f082b30a326ac6638c3e
Victor Jacob <victor.jacob@sap.com>
2024-06-23 17:53:41 +0200

    feat: Continous Logging (#14)
    * return and print more than one error per checked line
    
    * Added Logger class and continous logging to avoid losing logs in case of unexpected error during validation

--- src/points/points_csv_validator.py
+++ src/points/points_csv_validator.py
@@ -5,59 +5,77 @@ from src.core.validator import Validator
 class PointsValidator(Validator):
     points_columns = ["userId", "pointsToSpend", "statusPoints", "cashback", "allocatedAt", "expireAt", "setPlanExpiration", "reason", "title", "description"]
 
-    def __init__(self, csv_path, expected_columns=points_columns):
-        super().__init__(csv_path=csv_path, expected_columns=expected_columns)
+    def __init__(self, csv_path, log_path, expected_columns=points_columns):
+        super().__init__(csv_path=csv_path, log_path=log_path, expected_columns=expected_columns)
 
     def _validate_row(self, values):
+        errors = []
         if len(values) != len(self.expected_columns):
-            return False, f"Row should have {len(self.expected_columns)} columns"
+            error_message = f"Row should have {len(self.expected_columns)} columns"
+            errors = errors + [error_message]
+            # If there are not enough fields in the row there is no need for further validation
+            return False, errors
 
         # Extract the required values
         points_to_spend, status_points, cashback = values[1], values[2], values[3]
-        
-        # Check if at least one of the values is valid and positive
-        valid_pts = (points_to_spend.isdigit() and int(points_to_spend) > 0)
-        valid_status = (status_points.isdigit() and int(status_points) > 0)
-        try:
-            valid_cashback = (float(cashback) >= 0)
-        except ValueError:
-            valid_cashback = False
 
-        if not (valid_pts or valid_status or valid_cashback):
-            return False, "At least one of 'pointsToSpend', 'statusPoints', or 'cashback' must have a valid positive value."
-
-        # Ensure no negative values
-        if (points_to_spend and int(points_to_spend) < 0) or (status_points and int(status_points) < 0) or (valid_cashback and float(cashback) < 0):
-            return False, "Negative values are not allowed."
-        
         # Ensure correct data types
         if points_to_spend and not points_to_spend.isdigit():
-            return False, "Column 'pointsToSpend' should be an integer."
+            error_message = "Column 'pointsToSpend' should be an integer."
+            errors = errors + [error_message]
         if status_points and not status_points.isdigit():
-            return False, "Column 'statusPoints' should be an integer."
+            error_message = "Column 'statusPoints' should be an integer."
+            errors = errors + [error_message]
         if cashback:
             try:
                 float(cashback)
             except ValueError:
-                return False, "Column 'cashback' should be a float."
+                error_message = "Column 'cashback' should be a float."
+                errors = errors + [error_message]
 
+        # None of the points columns contain invalid data types
+        if len(errors) == 0:
+            # Check if at least one of the values is valid and positive
+            valid_pts = (points_to_spend.isdigit() and int(points_to_spend) > 0)
+            valid_status = (status_points.isdigit() and int(status_points) > 0)
+            try:
+                valid_cashback = (float(cashback) >= 0)
+            except ValueError:
+                valid_cashback = False
+
+            if not (valid_pts or valid_status or valid_cashback):
+                error_message = "At least one of 'pointsToSpend', 'statusPoints', or 'cashback' must have a valid positive value."
+                errors = errors + [error_message]
+            
+            # Ensure no negative values
+            if (points_to_spend and int(points_to_spend) < 0) or (status_points and int(status_points) < 0) or (valid_cashback and float(cashback) < 0):
+                error_message = "Negative values are not allowed."
+                errors = errors + [error_message]
+        
 
         # Validate expireAt and setPlanExpiration
         if values[6].lower() == "true":
             if values[5]:  # If there's a value in expireAt
-                return False, f"If setPlanExpiration is TRUE, expireAt should be empty"
+                error_message = f"If setPlanExpiration is TRUE, expireAt should be empty"
+                errors = errors + [error_message]
         elif values[6].lower() == "false":
             try:
                 expiration = int(values[5])
                 valid, error_message = _is_unix_millisecond_timestamp(expiration)
                 if not valid:
-                    return False, error_message
+                    errors = errors + [error_message]
                 past, error_message = _is_past_timestamp(expiration)
                 if past:
-                    return False, f"expireAt should be a future UNIX timestamp in milliseconds when setPlanExpiration is FALSE"
+                    error_message = f"expireAt should be a future UNIX timestamp in milliseconds when setPlanExpiration is FALSE"
+                    errors = errors + [error_message]
             except ValueError:
-                return False, f"expireAt should be an integer (UNIX timestamp in milliseconds) when setPlanExpiration is FALSE"
+                error_message = f"expireAt should be an integer (UNIX timestamp in milliseconds) when setPlanExpiration is FALSE"
+                errors = errors + [error_message]
         else:
-            return False, f"setPlanExpiration should be either TRUE or FALSE"
+            error_message = f"setPlanExpiration should be either TRUE or FALSE"
+            errors = errors + [error_message]
 
+        if len(errors) > 0:
+            return False, errors
+        
         return True, ""
